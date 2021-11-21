b74cf69d8a994499195aa7bec51cbbf841404d71
xumixuanyue <90192841+xumixuanyue@users.noreply.github.com>
2021-11-21 15:52:31 +0800

    Add files via upload

---
+++ scudb_1/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/xhy/Desktop/Database/scudb_1
+# Build directory: /home/xhy/Desktop/Database/scudb_1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
---
+++ scudb_1/build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xhy/Desktop/Database/scudb_1/src
+# Build directory: /home/xhy/Desktop/Database/scudb_1/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
