0b4d7386115a61520acc8bdf78b2758b74e9302a
xumixuanyue <90192841+xumixuanyue@users.noreply.github.com>
2021-11-21 15:55:03 +0800

    Delete scudb_1/build directory

--- scudb_1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xhy/Desktop/Database/scudb_1
-# Build directory: /home/xhy/Desktop/Database/scudb_1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
